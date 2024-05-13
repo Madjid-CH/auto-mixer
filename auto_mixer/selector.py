36faf3c02d12ca2586e73bc0ae251f011e55a3f3
Madjid-CH <a.chergui@esi-sba.dz>
2024-05-13 08:53:59 +0200

    added text modality selection

--- auto_mixer/selector.py
+++ auto_mixer/selector.py
@@ -1,10 +1,10 @@
 import os
 
+import pytorch_lightning as pl
 import torch
 from omegaconf import OmegaConf
 
-from auto_mixer.modules.image_module import MulticlassImageMixer, MultilabelImageMixer
-import pytorch_lightning as pl
+from auto_mixer.modules import MulticlassImageMixer, MultilabelImageMixer, MulticlassTextMixer, MultilabelTextMixer
 
 
 def select_encoder_for(modality, task, train_dataloader, val_dataloader):
@@ -17,7 +17,7 @@ def select_image_encoder(task, train_dataloader, val_dataloader):
     modules_configs_files = os.listdir("auto_mixer/cfg/image_models")
     cfgs = [OmegaConf.load(cfg_file) for cfg_file in modules_configs_files]
     train_cfg = OmegaConf.load("auto_mixer/cfg/micro_train.yml")
-    Mixer = get_model_for(task)
+    Mixer = get_image_model_for(task)
     target_length = len(train_dataloader.dataset[0]['labels'])
     models = {
         cfg.block_type: Mixer(
@@ -39,7 +39,7 @@ def select_image_encoder(task, train_dataloader, val_dataloader):
     return block_type, best_model.backbone
 
 
-def get_model_for(task):
+def get_image_model_for(task):
     if task == "multiclass":
         return MulticlassImageMixer
     elif task == "multilabel":
@@ -49,7 +49,38 @@ def get_model_for(task):
 
 
 def select_text_encoder(task, train_dataloader, val_dataloader):
-    pass
+    modules_configs_files = os.listdir("auto_mixer/cfg/text_models")
+    cfgs = [OmegaConf.load(cfg_file) for cfg_file in modules_configs_files]
+    train_cfg = OmegaConf.load("auto_mixer/cfg/micro_train.yml")
+    Mixer = get_text_model_for(task)
+    target_length = len(train_dataloader.dataset[0]['labels'])
+    models = {
+        cfg.block_type: Mixer(
+            target_length=target_length, model_cfg=cfg, optimizer_cfg=train_cfg.optimzer
+        ) for cfg in cfgs
+    }
+    trainer = pl.Trainer(
+        devices=torch.cuda.device_count(),
+        log_every_n_steps=train_cfg.log_interval_steps,
+        max_epochs=train_cfg.epochs
+    )
+
+    for model in models.values():
+        trainer.fit(model, train_dataloader, val_dataloader)
+        results = trainer.test(model, val_dataloader)
+        model.results = results
+
+    block_type, best_model = max(models.items(), key=lambda x: x[1].results['test_accuracy'])
+    return block_type, best_model.backbone
+
+
+def get_text_model_for(task):
+    if task == "multiclass":
+        return MulticlassTextMixer
+    elif task == "multilabel":
+        return MultilabelTextMixer
+    else:
+        raise ValueError(f"Unknown task: {task}")
 
 
 selectors = {
