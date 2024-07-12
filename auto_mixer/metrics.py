57d9707d4d0511c74b3e474713a3b457edaab148
Madjid-CH <a.chergui@esi-sba.dz>
2024-07-12 15:13:12 +0200

    refactoring

--- auto_mixer/metrics.py
+++ auto_mixer/metrics.py
@@ -6,32 +6,32 @@ _Task = Literal["binary", "multiclass", "multilabel"]
 
 
 def get_multiclass_metrics(num_classes):
-    task: _Task = 'multiclass'
+    t: _Task = 'multiclass'
     return dict(
-        accuracy=tm.Accuracy(task=task, num_classes=num_classes),
-        precision_macro=tm.Precision(task=task, num_classes=num_classes, average='macro'),
-        precision_micro=tm.Precision(task=task, num_classes=num_classes, average='micro'),
-        f1_macro=tm.F1Score(task=task, num_classes=num_classes, average='macro'),
-        f1_micro=tm.F1Score(task=task, num_classes=num_classes, average='micro'),
-        f1_weighted=tm.F1Score(task=task, num_classes=num_classes, average='weighted'),
-        recall_macro=tm.Recall(task=task, num_classes=num_classes, average='macro'),
-        recall_micro=tm.Recall(task=task, num_classes=num_classes, average='micro'),
+        accuracy=tm.Accuracy(task=t, num_classes=num_classes),
+        precision_macro=tm.Precision(task=t, num_classes=num_classes, average='macro'),
+        precision_micro=tm.Precision(task=t, num_classes=num_classes, average='micro'),
+        f1_macro=tm.F1Score(task=t, num_classes=num_classes, average='macro'),
+        f1_micro=tm.F1Score(task=t, num_classes=num_classes, average='micro'),
+        f1_weighted=tm.F1Score(task=t, num_classes=num_classes, average='weighted'),
+        recall_macro=tm.Recall(task=t, num_classes=num_classes, average='macro'),
+        recall_micro=tm.Recall(task=t, num_classes=num_classes, average='micro'),
     )
 
 
 def get_multilabel_metrics(num_labels):
-    task: _Task = 'multilabel'
+    t: _Task = 'multilabel'
     return dict(
-        accuracy=tm.Accuracy(task=task, num_labels=num_labels),
-        auroc_macro=tm.AUROC(task=task, num_labels=num_labels, average='macro'),
-        auroc_weighted=tm.AUROC(task=task, num_labels=num_labels, average='weighted'),
-        precision_macro=tm.Precision(task=task, num_labels=num_labels, average='macro'),
-        precision_micro=tm.Precision(task=task, num_labels=num_labels, average='micro'),
-        f1_macro=tm.F1Score(task=task, num_labels=num_labels, average='macro'),
-        f1_micro=tm.F1Score(task=task, num_labels=num_labels, average='micro'),
-        f1_weighted=tm.F1Score(task=task, num_labels=num_labels, average='weighted'),
-        recall_macro=tm.Recall(task=task, num_labels=num_labels, average='macro'),
-        recall_micro=tm.Recall(task=task, num_labels=num_labels, average='micro'),
+        accuracy=tm.Accuracy(task=t, num_labels=num_labels),
+        auroc_macro=tm.AUROC(task=t, num_labels=num_labels, average='macro'),
+        auroc_weighted=tm.AUROC(task=t, num_labels=num_labels, average='weighted'),
+        precision_macro=tm.Precision(task=t, num_labels=num_labels, average='macro'),
+        precision_micro=tm.Precision(task=t, num_labels=num_labels, average='micro'),
+        f1_macro=tm.F1Score(task=t, num_labels=num_labels, average='macro'),
+        f1_micro=tm.F1Score(task=t, num_labels=num_labels, average='micro'),
+        f1_weighted=tm.F1Score(task=t, num_labels=num_labels, average='weighted'),
+        recall_macro=tm.Recall(task=t, num_labels=num_labels, average='macro'),
+        recall_micro=tm.Recall(task=t, num_labels=num_labels, average='micro'),
     )
 
 
---
+++ run_for_signifigance.py
@@ -0,0 +1,55 @@
+import importlib
+
+import pytorch_lightning as pl
+import torch
+from omegaconf import OmegaConf
+from pytorch_lightning.loggers import WandbLogger
+
+from auto_mixer.datasets.mimic_cxr.mimic_cxr import MIMICCXRDataModule
+from auto_mixer.runner import find_architecture
+
+
+def main():
+    data = MIMICCXRDataModule(batch_size=64, num_workers=4)
+    data.setup()
+    pl.seed_everything(42)
+    for run in range(1):
+        fusion_function, best_model = find_architecture(data)
+        cfg = OmegaConf.load("auto_mixer/cfg/train.yml")
+        wandb_logger = WandbLogger(project='auto-mixer', name=f'mimic_cxr - {run}')
+        wandb_logger.experiment.config.update(cfg)
+        callbacks = build_callbacks(cfg.callbacks)
+        trainer = pl.Trainer(
+            callbacks=callbacks,
+            devices=torch.cuda.device_count(),
+            log_every_n_steps=cfg.log_interval_steps,
+            logger=wandb_logger,
+            max_epochs=cfg.epochs
+        )
+
+        train_dataloader = data.train_dataloader()
+        val_dataloader = data.val_dataloader()
+        print("\nStarting full training...\n")
+        print(best_model)
+        trainer.fit(best_model, train_dataloader, val_dataloader)
+        results = trainer.test(best_model, val_dataloader)[0]
+        print(results)
+
+
+def build_callbacks(callbacks_cfg):
+    callbacks = []
+    for cb in callbacks_cfg:
+        callbacks.append(build_callback(cb))
+    return callbacks
+
+
+def build_callback(cfg):
+    module_path, class_name = cfg.class_path.rsplit(".", 1)
+    module = importlib.import_module(module_path)
+    cls = getattr(module, class_name)
+    callback = cls(**cfg.init_args)
+    return callback
+
+
+if __name__ == '__main__':
+    main()
