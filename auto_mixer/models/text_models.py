f1ac09a1c2cb5f9c16e1a9e7cbdf2e0d6a92f82b
Madjid-CH <a.chergui@esi-sba.dz>
2024-07-17 10:38:47 +0200

    fixed input dim

--- auto_mixer/models/text_models.py
+++ auto_mixer/models/text_models.py
@@ -12,7 +12,7 @@ class MultilabelTextMixer(MultilabelMixer):
         model_cls = getattr(modules, model_cfg.block_type)
         self.backbone = model_cls(hidden_dim=hidden_dim, patch_size=patch_size, **model_cfg.text,
                                   dropout=model_cfg.dropout)
-        classifier_input_dim = model_cfg.get('classifier_input_dim', 512)
+        classifier_input_dim = model_cfg.get('classifier_input_dim', 301)
         self.backbone.classifier_input_dim = classifier_input_dim
         self.classifier = TwoLayeredPerceptron(input_dim=classifier_input_dim, hidden_dim=512,
                                                output_dim=self.target_length)
